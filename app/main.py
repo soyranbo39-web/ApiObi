5d49fa6eb8ce2ee68a3f14839f4b9567a007a469
kevinrosaldo <soy.ranbo39@gmail.com>
2026-05-10 02:36:48 -0600

    dd

--- app/main.py
+++ app/main.py
@@ -98,4 +98,4 @@ app = create_app()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=False)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
